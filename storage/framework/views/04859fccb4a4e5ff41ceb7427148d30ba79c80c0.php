156c7cf495dc828c790d1ce1e8eb16fa43033e0c
FacundoaQuiroga <facundoaquiroga1@gmail.com>
2021-11-21 18:07:20 -0300

    force

---
+++ storage/framework/views/04859fccb4a4e5ff41ceb7427148d30ba79c80c0.php
@@ -0,0 +1,8 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    
+    <contact />
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/facundo/laravel-vue-crud/resources/views/home.blade.php ENDPATH**/ ?>
