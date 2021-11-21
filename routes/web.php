1bd0ba2d8247251210b28bdf919279ffc55a734e
FacundoaQuiroga <facundoaquiroga1@gmail.com>
2021-11-21 17:42:05 -0300

    auth

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::apiResource('/contacts', ContactController::class);
+
+Auth::routes();
